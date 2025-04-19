f9c6be74d35d474e78b0cfd4e27eaff30730648a
omuletzu <mihneasimota1234@gmail.com>
2025-04-19 18:03:56 +0300

    first commit

---
+++ src/main/java/com/example/be_accesa/BeAccesaApplication.java
@@ -0,0 +1,13 @@
+package com.example.be_accesa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeAccesaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeAccesaApplication.class, args);
+	}
+
+}
