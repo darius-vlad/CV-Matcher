1ab127f552857d4d8eaef3749b4a92f82ecd7fe1
omuletzu <mihneasimota1234@gmail.com>
2025-04-20 19:41:29 +0300

    added redis connection

--- src/main/java/com/example/be_accesa/Controller/Controller.java
+++ src/main/java/com/example/be_accesa/Controller/Controller.java
@@ -2,7 +2,9 @@ package com.example.be_accesa.Controller;
 
 import com.example.be_accesa.DTO.CvDTO;
 import com.example.be_accesa.Service.FilebaseService;
+import com.example.be_accesa.Service.RedisService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -15,17 +17,21 @@ import java.util.UUID;
 public class Controller {
 
     private final FilebaseService filebaseService;
+    private final RedisService redisService;
 
     @Autowired
-    public Controller(FilebaseService filebaseService) {
+    public Controller(FilebaseService filebaseService,
+                      RedisService redisService) {
         this.filebaseService = filebaseService;
+        this.redisService = redisService;
     }
 
     @PostMapping("/upload-batch")
     public void uploadBatch(@RequestParam("files") MultipartFile[] files) {
         for(MultipartFile file : files) {
-            String randomId = "cv-raw/" + UUID.randomUUID().toString() + ".pdf";
-            filebaseService.uploadRawCv(randomId, file);
+            String cvId = "cv-raw/" + UUID.randomUUID().toString() + ".pdf";
+            filebaseService.uploadRawCv(cvId, file);
+            redisService.enqueueCvId(cvId);
         }
     }
 
---
+++ src/main/java/com/example/be_accesa/Service/RedisService.java
@@ -0,0 +1,27 @@
+package com.example.be_accesa.Service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RedisService {
+    private static final Logger logger = LoggerFactory.getLogger(RedisService.class);
+
+    @Autowired
+    private StringRedisTemplate template;
+
+    private static final String REDIS_CHANNEL = "queue";
+
+    public void enqueueCvId(String cvId) {
+        try {
+            template.opsForList().leftPush(REDIS_CHANNEL, cvId);
+            logger.info("Pushed CV id : " + cvId);
+        }
+        catch (Exception e) {
+            logger.error("Error pushing CV id :" + cvId);
+        }
+    }
+}
